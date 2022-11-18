ce1cc7181cde4ef7df9103b997921701479f664a
Mohammad Mc <mohammad.chitsaz80@gmail.com>
2022-11-19 01:58:24 +0330

    Digitize app project

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,27 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./public/**/*.html"],
+  content: ["./public/**/*.{html,js}"],
   theme: {
     screens: {
-      'xs': '475px',
+      xs: "475px",
       // => @media (min-width: 475px) { ... }
 
-      'sm': '640px',
+      sm: "640px",
       // => @media (min-width: 640px) { ... }
 
-      'md': '768px',
+      md: "768px",
       // => @media (min-width: 768px) { ... }
 
-      'lg': '1024px',
+      lg: "1024px",
       // => @media (min-width: 1024px) { ... }
 
-      'xl': '1280px',
+      xl: "1280px",
       // => @media (min-width: 1280px) { ... }
 
-      '2xl': '1536px',
+      "2xl": "1536px",
       // => @media (min-width: 1536px) { ... }
     },
-    
+
     extend: {
       fontFamily: {
         sans: ["iranYekan"],
@@ -29,7 +29,13 @@ module.exports = {
       boxShadow: {
         "3xl": ["rgba(0, 0, 0, 0.35) 0px 1px 10px"],
       },
+      boxShadow: {
+        't': '0 -10px 40px rgba(0, 0, 0, 0.3)',
+      }
     },
   },
-  plugins: [],
+  plugins: [
+    require("@tailwindcss/forms"),
+    // ...
+  ],
 };
